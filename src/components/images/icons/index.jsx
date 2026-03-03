79d00268128b2cc702d550439e79a1d55beac1f0
Lukasz Nogaj <lukanogaj@hotmail.co.uk  git config --global user.name lukanogaj>
2026-03-03 05:28:09 +0000

    Polish TodoRow date/time responsiveness

--- src/components/images/icons/index.jsx
+++ src/components/images/icons/index.jsx
@@ -296,21 +296,18 @@ export const Dots = ({ className, ...props }) => (
 		/>
 	</svg>
 );
+
 export const Dot = ({ className, ...props }) => (
 	<svg
 		xmlns='http://www.w3.org/2000/svg'
 		viewBox='0 0 24 24'
-		fill='none'
-		stroke='currentColor'
-		strokeWidth={2}
-		strokeLinecap='round'
-		strokeLinejoin='round'
+		fill='currentColor'
 		className={className}
 		{...props}>
 		<circle
 			cx='12'
 			cy='12'
-			r='1'
+			r='6'
 		/>
 	</svg>
 );
